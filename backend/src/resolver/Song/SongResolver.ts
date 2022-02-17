b60640ae99a14537dc1c85782432056d1ace95af
lightyears <lightyears1998@hotmail.com>
2022-02-18 02:35:37 +0800

    你的斑驳 与众不同

---
+++ backend/src/resolver/Song/SongResolver.ts
@@ -0,0 +1,22 @@
+import {
+  Query, Resolver, Ctx, Arg, Int, Authorized
+} from "type-graphql";
+import { InjectManager, InjectRepository } from "typeorm-typedi-extensions";
+import { EntityManager } from "typeorm";
+
+import { User } from "../../entity";
+import { AppUserContext } from "../../context";
+import { UserRepository } from "../../repo";
+import { UsersConnection } from "../../type";
+import { Song } from "../../entity/Song";
+
+@Resolver(() => Song)
+export class UserResolver {
+  @InjectManager()
+  private readonly manager!: EntityManager
+
+  @Query(() => [Song])
+  async songs(): Promise<Song[]> {
+    return this.manager.find(Song);
+  }
+}
