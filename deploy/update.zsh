0e0b8b7a724355e0916a03b6a9771c9d220fa83a
lightyears <lightyears1998@hotmail.com>
2022-02-18 01:56:33 +0800

    初养成

--- deploy/update.zsh
+++ deploy/update.zsh
@@ -4,11 +4,11 @@ print_pwd () {
     echo "Entering $PWD:"
 }
 
-PM2_NAME_PREFIX=`git branch --show-current`
+PM2_PROJECT_NAME=linguistics
 
-if [ -z "$PM2_NAME_PREFIX" ];
-  then echo "$PM2_NAME_PREFIX is null" && exit 1;
-  else echo "PM2_NAME_PREFIX=$PM2_NAME_PREFIX";
+if [ -z "$PM2_PROJECT_NAME" ];
+  then echo "$PM2_PROJECT_NAME is null" && exit 1;
+  else echo "PM2_PROJECT_NAME=$PM2_PROJECT_NAME";
 fi
 
 setopt verbose
@@ -16,5 +16,5 @@ setopt verbose
 git reset --hard
 git pull --rebase
 
-(cd ../backend && print_pwd && yarn install && yarn build && pm2 restart $PM2_NAME_PREFIX-backend)
-(cd ../frontend && print_pwd && yarn install && yarn build && pm2 restart $PM2_NAME_PREFIX-frontend)
+(cd ../backend && print_pwd && yarn install && yarn build && pm2 restart $PM2_PROJECT_NAME-backend)
+(cd ../frontend && print_pwd && yarn install && yarn build && pm2 restart $PM2_PROJECT_NAME-frontend)
